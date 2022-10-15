a0f9b85194d39a6b7349a600be50dfec72ce58ba
DESKTOP-0ES0SMT\Aleksa <aojdanic97@gmail.com>
2022-10-15 17:54:59 +0200

    Druga nedelja

---
+++ druga_nedelja/vezba.js
@@ -0,0 +1,49 @@
+//prvi zadatak
+
+var p = true;
+var q = true;
+
+console.log(p && q);
+console.log(p && !q);
+console.log(!p && q);
+console.log(!p && !q);
+
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//drugi zadatak
+
+const age = 25;
+console.log(age < 0);
+console.log(age > 120);
+
+//treci zadatak
+
+const speed = 130
+if (speed >= 60 && speed <= 120) {
+    console.log('we are driving safely')
+} else {
+    console.log('we are not driving safely')
+};
+
+
+//variable declaration and initialization
+
+//prvi zadatak
+
+var moodToday;
+var $mood;
+var mood;
+
+
+
+
+
+//drugi zadatak
+
+var currentMood = 'I am feeling well';
+console.log(currentMood);
+
