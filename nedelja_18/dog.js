4cb887be47c9bd226451a8813fcedbda03c84d64
DESKTOP-0ES0SMT\Aleksa <aojdanic97@gmail.com>
2023-02-05 02:34:56 +0100

    used promises for first time (amazing)

--- nedelja_18/dog.js
+++ nedelja_18/dog.js
@@ -5,21 +5,36 @@
 const button = document.querySelector(".button");
 const div = document.querySelector("div");
 
-const getDogPic = function () {
-  let xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "https://dog.ceo/api/breeds/image/random");
-  xmlHttp.onload = function () {
-    let data = JSON.parse(xmlHttp.responseText);
-    console.log(data);
-    let html = `
-    <img src="${data.message}"/>`;
-    div.insertAdjacentHTML("afterbegin", html);
-  };
-  xmlHttp.send();
+// const getDogPic = function () {
+//   let xmlHttp = new XMLHttpRequest();
+//   xmlHttp.open("GET", "https://dog.ceo/api/breeds/image/random");
+//   xmlHttp.onload = function () {
+//     let data = JSON.parse(xmlHttp.responseText);
+//     console.log(data);
+//     let html = `
+//     <img src="${data.message}"/>`;
+//     div.insertAdjacentHTML("afterbegin", html);
+//   };
+//   xmlHttp.send();
+// };
+
+//using promises
+
+/////////////////////////////////////////////////////////////////////
+
+const createPic = function (data) {
+  let html = `
+  <img src="${data.message}"/>`;
+  div.insertAdjacentHTML("afterbegin", html);
 };
 
+const getDogPic = function () {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => createPic(data));
+};
+
+/////////////////////////////////////////////////////////////////////
+
 // Event listeners
-button.addEventListener("click", function (e) {
-  e.preventDefault();
-  getDogPic();
-});
+button.addEventListener("click", getDogPic);
