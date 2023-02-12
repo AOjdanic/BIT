06ffa718e9714e6d4d525ceb9ca08527c36d22e0
DESKTOP-0ES0SMT\Aleksa <aojdanic97@gmail.com>
2023-02-12 11:18:55 +0100

    svi zadaci iz nedelje 19

--- nedelja_19/BitShow/js/bitshow.js
+++ nedelja_19/BitShow/js/bitshow.js
@@ -4,29 +4,23 @@ const search = document.querySelector("input");
 const div = document.querySelector(".container");
 const title = document.querySelector(".title");
 const popular = document.querySelector(".popular");
+
 const displayMovie = function (obj) {
   let html = `
-  <div data-id="${obj.externals.thetvdb}" data-src="${obj.id}" class="card" style="width: 18rem;">
-  <img class="card-img-top" src="${obj.image?.medium}" alt="Card image cap">
+  <div data-id="${
+    obj?.show?.externals?.thetvdb ?? obj?.externals?.thetvdb
+  }" data-src="${obj?.show?.id ?? obj?.id}" class="card" style="width: 18rem;">
+  <img class="card-img-top" src="${
+    obj?.show?.image?.medium ?? obj?.image?.medium
+  }" alt="Card image cap">
   <div class="card-body">
-    <h5 class="card-title">${obj.name}</h5>
+    <h5 class="card-title">${obj?.show?.name ?? obj?.name}</h5>
    </div>
 </div>`;
   div.insertAdjacentHTML("beforeend", html);
 };
 
-const displayMovie2 = function (obj) {
-  let html = `
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${obj.show.image?.medium}" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">${obj.show.name}</h5>
-     </div>
-  </div>`;
-  div.insertAdjacentHTML("beforeend", html);
-};
-
-window.addEventListener("load", function (e) {
+const fetchTop50 = function () {
   fetch("https://api.tvmaze.com/shows")
     .then((result) => result.json())
     .then((result) => {
@@ -37,16 +31,22 @@ window.addEventListener("load", function (e) {
       console.log(top50);
       return top50.forEach((obj) => displayMovie(obj));
     });
-});
+};
+
+const removeCards = function () {
+  return document.querySelectorAll(".card").forEach((el) => el?.remove());
+};
+
+window.addEventListener("load", fetchTop50);
 
 search.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
-    document.querySelectorAll(".card").forEach((el) => el?.remove());
+    removeCards();
     fetch(`https://api.tvmaze.com/search/shows?q=${search.value}`)
       .then((result) => result.json())
       .then((result) => {
         let list = Array.from(result);
-        list.forEach((obj) => displayMovie2(obj));
+        list.forEach((obj) => displayMovie(obj));
       });
     search.value = "";
   }
@@ -55,17 +55,8 @@ search.addEventListener("keypress", function (e) {
 title.addEventListener("click", function (e) {
   popular.textContent = "Popular Shows";
   document.querySelector(".single_movie")?.remove();
-  document.querySelectorAll(".card").forEach((el) => el?.remove());
-  fetch("https://api.tvmaze.com/shows")
-    .then((result) => result.json())
-    .then((result) => {
-      let list = Array.from(result);
-      let top50 = list
-        .sort((a, b) => b.rating.average - a.rating.average)
-        .slice(0, 50);
-
-      return top50.forEach((obj) => displayMovie(obj));
-    });
+  removeCards();
+  fetchTop50();
 });
 
 div.addEventListener("click", function (e) {
@@ -89,5 +80,5 @@ div.addEventListener("click", function (e) {
   fetch(`https://api.tvmaze.com/shows/${target.dataset.src}/seasons`)
     .then((seasons) => seasons.json())
     .then((season) => console.log(season));
-  document.querySelectorAll(".card").forEach((el) => el.remove());
+  removeCards();
 });
