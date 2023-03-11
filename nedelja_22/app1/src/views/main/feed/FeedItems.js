a04dbb2c715623891caa7500b401f84bdc8b2408
DESKTOP-0ES0SMT\Aleksa <aojdanic97@gmail.com>
2023-03-11 15:36:53 +0100

    svi zadaci iz nedelje 22

---
+++ nedelja_22/app1/src/views/main/feed/FeedItems.js
@@ -0,0 +1,9 @@
+import SingleItem from "./SingleItem.js";
+
+export default function FeedItems() {
+  return (
+    <div>
+      <SingleItem></SingleItem>
+    </div>
+  );
+}
---
+++ nedelja_22/app1/src/views/main/feed/SingleItem.js
@@ -0,0 +1,9 @@
+export default function SingleItem() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h3>Heading 3</h3>
+      <p></p>
+    </div>
+  );
+}
