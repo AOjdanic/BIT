b5cbd7342b4b4b6457a48c41f1695d99cb949c8d
DESKTOP-0ES0SMT\Aleksa <aojdanic97@gmail.com>
2023-03-19 23:26:22 +0100

    dynamic rendering

--- nedelja_23/user-list/src/components/Cards.js
+++ nedelja_23/user-list/src/components/Cards.js
@@ -3,42 +3,14 @@ import "./Cards.css";
 export default function Cards(props) {
   return (
     <div className="cards">
-      <Card
-        image={props.users[0].imageMed}
-        name={props.users[0].name}
-        email={props.users[0].email}
-        birthDate={props.users[0].birthDate}
-      />
-      <Card
-        image={props.users[1].imageMed}
-        name={props.users[1].name}
-        email={props.users[1].email}
-        birthDate={props.users[1].birthDate}
-      />
-      <Card
-        image={props.users[2].imageMed}
-        name={props.users[2].name}
-        email={props.users[2].email}
-        birthDate={props.users[2].birthDate}
-      />
-      <Card
-        image={props.users[3].imageMed}
-        name={props.users[3].name}
-        email={props.users[3].email}
-        birthDate={props.users[3].birthDate}
-      />
-      <Card
-        image={props.users[4].imageMed}
-        name={props.users[4].name}
-        email={props.users[4].email}
-        birthDate={props.users[4].birthDate}
-      />
-      <Card
-        image={props.users[5].imageMed}
-        name={props.users[5].name}
-        email={props.users[5].email}
-        birthDate={props.users[5].birthDate}
-      />
+      {props.users.map((user) => (
+        <Card
+          image={user.imageMed}
+          name={user.name}
+          email={user.email}
+          birthDate={user.birthDate}
+        />
+      ))}
     </div>
   );
 }
--- nedelja_23/user-list/src/components/Users.js
+++ nedelja_23/user-list/src/components/Users.js
@@ -3,42 +3,14 @@ import UserPost from "./UserPost.js";
 export default function Users(props) {
   return (
     <div>
-      <UserPost
-        image={props.users[0].image}
-        name={props.users[0].name}
-        email={props.users[0].email}
-        birthDate={props.users[0].birthDate}
-      />
-      <UserPost
-        image={props.users[1].image}
-        name={props.users[1].name}
-        email={props.users[1].email}
-        birthDate={props.users[1].birthDate}
-      />
-      <UserPost
-        image={props.users[2].image}
-        name={props.users[2].name}
-        email={props.users[2].email}
-        birthDate={props.users[2].birthDate}
-      />
-      <UserPost
-        image={props.users[3].image}
-        name={props.users[3].name}
-        email={props.users[3].email}
-        birthDate={props.users[3].birthDate}
-      />
-      <UserPost
-        image={props.users[4].image}
-        name={props.users[4].name}
-        email={props.users[4].email}
-        birthDate={props.users[4].birthDate}
-      />
-      <UserPost
-        image={props.users[5].image}
-        name={props.users[5].name}
-        email={props.users[5].email}
-        birthDate={props.users[5].birthDate}
-      />
+      {props.users.map((user) => (
+        <UserPost
+          image={user.image}
+          name={user.name}
+          email={user.email}
+          birthDate={user.birthDate}
+        />
+      ))}
     </div>
   );
 }
